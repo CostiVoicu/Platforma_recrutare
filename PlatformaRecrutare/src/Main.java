4e4d1a48a4210e451911ba118b07e236ac104490
Constantin Voicu <voicu.constantin@outlook.com>
2023-12-19 14:54:08 +0200

    Added MainWindow class

--- PlatformaRecrutare/src/Main.java
+++ PlatformaRecrutare/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MainWindow window = new MainWindow();
+        DButils db = new DButils();
+        db.connect();
     }
 }
---
+++ PlatformaRecrutare/src/MainWindow.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setSize(600, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Main Window");
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
